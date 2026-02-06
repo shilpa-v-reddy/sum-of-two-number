a145e93fd0fecd68496ef3280fc6a5d815578476
shilpa-v-reddy <shilpavreddy011@gmail.com>
2026-02-06 11:30:08 +0530

    Add files via upload

---
+++ sum1.py
@@ -0,0 +1,14 @@
+def getSum(number1,number2):
+    return number1+number2
+
+def invoke_getSum():
+    sum = getSum(5,5)
+    print("sum of two numbers is", sum)
+
+    sum1 = getSum(1.5, 1.5)
+    print("sum of two numbers is", sum1)
+
+    sum2= getSum("Hello", "World")
+    print("sum of two numbers is", sum2)
+
+invoke_getSum()
