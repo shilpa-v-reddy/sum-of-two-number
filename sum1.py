bdf2662ebc75ee1dca9838a0be38309d25b9fd02
shilpa-v-reddy <shilpavreddy011@gmail.com>
2026-02-06 11:37:04 +0530

    Update sum1.py

--- sum1.py
+++ sum1.py
@@ -1,5 +1,5 @@
-def getSum(number1,number2):
-    return number1+number2
+def getSum(number1, number2):
+    return number1 + number2
 
 def invoke_getSum():
     sum = getSum(5,5)
@@ -12,3 +12,4 @@ def invoke_getSum():
     print("sum of two numbers is", sum2)
 
 invoke_getSum()
+
